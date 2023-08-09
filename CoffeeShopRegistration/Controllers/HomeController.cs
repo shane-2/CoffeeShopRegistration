d132e9734e538011d912c1279626492c1555b713
shane <shanechastain1@ymail.com>
2023-08-09 17:02:02 -0400

    first

---
+++ CoffeeShopRegistration/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace CoffeeShopRegistration.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        UserList Use = new UserList();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Result(User c)
+        {
+            Use.AddNew(c);
+
+            return View(c);
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShopRegistration/Models/UserList.cs
@@ -0,0 +1,19 @@
+﻿namespace CoffeeShopRegistration.Models
+{
+    public class UserList
+    {
+        public List<User> Users = new List<User>();
+
+    //public UserList()
+    //{
+    //    Users.Add(new User());
+    //}
+
+
+        public void AddNew(User c)
+        {
+            Users.Add(c);
+        }
+    }
+}
+
